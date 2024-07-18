9c7c374d5b27df10d1aa4071d3cd9ccd1babd7e2
Ashim Poudyal <ashim1998.poudyal@gmail.com>
2024-07-18 13:04:14 +0545

    Initial

---
+++ client/src/pages/LoginPage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const LoginPage = () => {
+  return (
+    <div>
+      <form>
+        <input type="email" placeholder="Your email" />
+        <input type="password" placeholder="password" />
+        <button>Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default LoginPage;
